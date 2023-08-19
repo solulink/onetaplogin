1cf7d83b62bd9255124f7d5ee201c26d3d0878c0
toan trinh <solulink@gmail.com>
2023-08-19 11:33:50 -0500

    Initial change

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const {OAuth2Client} = require('google-auth-library');
+const clientId = "160707844309-e7s4djsi8p0troroa47vla30ojs01qlt.apps.googleusercontent.com"
+const client = new OAuth2Client(clientId)
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(
+    cors({
+       origin: 'http://localhost:3000',
+       methods: "GET,POSTPUT,DELETE,UPDATE",
+       credentials: false,
+       "preflightContinue": false,
+   }));
+const users = [];
+
+const upsert = (array, item) =>{
+    const i = array.findIndex((_item) => _item.email === item.email);
+    if (i > -1) array[i] = item;
+    else array.push(item)
+}
+
+app.post('/api/google-login', async (req,res) => {
+    const { token } = req.body;
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: clientId,
+    });
+
+    const {name, email, picture } = ticket.getPayload();
+    upsert(users, {name, email, picture});
+    res.status(201).json({name, email, picture});
+});
+
+app.listen(5000, () => {
+    console.log("Server port 5000");
+});
--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import googleOneTap from "google-one-tap"
+
+const options = {
+  client_id: "160707844309-e7s4djsi8p0troroa47vla30ojs01qlt.apps.googleusercontent.com",
+  auto_select: false,
+  cancel_on_tap_outside: false,
+  context: "signin",
+};
 
 function App() {
+  const [loginData, setLoginData] = useState(
+    localStorage.getItem("loginData")
+    ? JSON.parse(localStorage.getItem("loginData"))
+    :null
+  );
+    useEffect(()=>{
+      if(!loginData){
+        googleOneTap(options, async(response) => {
+          const res = await fetch("http://localhost:5000/api/google-login", {
+            method: "POST",
+            body: JSON.stringify({
+              token: response.credential,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+
+          const data = await res.json();
+          setLoginData(data);
+          localStorage.setItem("loginData", JSON.stringify(data));
+        })
+      }
+    },[loginData])
+
+    const handleLogout = () => {
+      localStorage.removeItem("loginData");
+      setLoginData(null);
+    }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Implement Google OneTap
+      <div>
+        {loginData ? (
+          <div>
+          <h3>
+            You "{loginData.name}" logged in as {loginData.email}
+          </h3>
+          <button onClick={handleLogout}> Logout</button>
+          </div>
+        ) : (
+          <div>Not logged in</div>
+        )}
+      </div>
       </header>
     </div>
   );
